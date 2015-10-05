82884b70227bf84999d99b02f3c5c2698f714e3a
Fred Gravil <fgravil18@gmail.com>
2015-10-05 15:18:15 -0400

    Networking for Unity through LAN connection (small game)

---
+++ Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Move : NetworkBehaviour
+{
+    public string axis = "Horizontal";
+    public float speed = 1.0f;
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //if this script is runnin on a local player, send this information.
+        if(isLocalPlayer)
+        {
+            //moves the player to the right based on input and time is based on seconds
+            transform.position += Input.GetAxis(axis) * Vector3.right * speed * Time.deltaTime;
+        }
+        
+	}
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Shoot : NetworkBehaviour
+{
+    public GameObject bullet;
+    public string button = "Fire1";
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            //prevents spam shooting
+            if (Input.GetButtonDown(button))
+            {
+                CmdShoot();
+            }
+        }
+        
+	}
+    
+    //command means the function is called on the server instead of locally
+    [Command]
+    void CmdShoot()
+    {
+        GameObject instance = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+        NetworkServer.Spawn(instance);
+    }
+}
